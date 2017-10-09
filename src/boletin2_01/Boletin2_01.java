19e87e3bb0714bcb419609b112edf461fc89ad60
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-09 19:30:37 +0200

    Ejercicio Finalizado

---
+++ src/boletin2_01/Boletin2_01.java
@@ -0,0 +1,11 @@
+package boletin2_01;
+
+public class Boletin2_01 {
+
+    public static void main(String[] args) {
+     float base = 4f, altura =3f, area;
+      area = base* altura /2;
+        System.out.println("Area = "+ area);
+    }
+    
+}
